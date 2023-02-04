4526d5217d2189222cdcfaea5d793413b64bd7b4
Marwan Mohammed <maranalhmde97@gmail.com>
2023-02-04 15:45:06 +0000

    ceate app styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -23,7 +23,7 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
           SizedBox(
             height: 16,
