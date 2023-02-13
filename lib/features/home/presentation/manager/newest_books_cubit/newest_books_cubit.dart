3ebd7b81d05f162c99f4e12d953e9c951d3412c0
Marwan Mohammed <maranalhmde97@gmail.com>
2023-02-13 13:17:57 +0000

    Create newest books cubit state

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
